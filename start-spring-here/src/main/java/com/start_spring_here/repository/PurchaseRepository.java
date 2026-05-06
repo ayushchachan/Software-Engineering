03fc540ea6dac050dfa30af477af9f4261d79bf6
Ayush Chachan <ayushchachan1999@gmail.com>
2026-05-06 20:28:04 +0530

    update

--- start-spring-here/src/main/java/com/start_spring_here/repository/PurchaseRepository.java
+++ start-spring-here/src/main/java/com/start_spring_here/repository/PurchaseRepository.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class PurchaseRepository {
-    // when sprint boot during runtime saw we added jdbc starter and a driver dependency
+    // when spring boot, during runtime, saw we added jdbc starter dependency and a driver dependency
     // it automatically created a JdbcTemplate and a DataSource instances. JdbcTemplate uses a DataSource object inside
     // its implementation while talking with a database
+    
+    //    JdbcTemplate
+    //    ↓
+    //    DataSource
+    //    ↓
+    //    JDBC Driver
+    //    ↓
+    //    Database
     private JdbcTemplate jdbcTemplate;
 
     public PurchaseRepository(JdbcTemplate jdbcTemplate) {
